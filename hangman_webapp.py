2c3d4fcfbe5d6c71a8fdfde0ed0e05a93fc0f9d0
Ubuntu <ubuntu@ip-172-31-47-192.us-east-2.compute.internal>
2019-12-22 06:02:06 +0000

    second commit

---
+++ hangman_webapp.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/main')
+def hello_world():
+    return render_template('hangman.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
