ed5f29ff80709241f779cc6794365f8d350288f9
Jeremiah Crowell <jcrowe6@illinois.edu>
2019-12-27 11:38:58 -0600

    Implemented keydown things

--- hangman_webapp.py
+++ hangman_webapp.py
@@ -7,3 +7,12 @@ def hello_world():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+# to refactor this:
+# take the existing hangman script and streamline it
+# 2 approaches:
+#   make one route/function that will take full word state and return list of letters/words
+#       this will require running through whole list of words every new letter (may be slow)
+#   use some sort of caching either in browser or in the server so that each session uses one list that shrinks
+#   over the course of using it
+#       more optimal but more difficult...
