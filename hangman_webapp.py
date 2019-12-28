5b474e96f862742bcbd78ecb3c7834dbda5436b1
Jeremiah Crowell <jcrowe6@illinois.edu>
2019-12-27 21:54:23 -0600

    Added word route

--- hangman_webapp.py
+++ hangman_webapp.py
@@ -8,6 +8,12 @@ def hello_world():
 if __name__ == '__main__':
     app.run(debug=True)
 
+# should take the current word in some format and return the new list of words in some format
+# right now just testing
+@app.route('/word')
+def createSuggestions():
+    return "helloooo"
+
 # to refactor this:
 # take the existing hangman script and streamline it
 # 2 approaches:
@@ -16,3 +22,4 @@ if __name__ == '__main__':
 #   use some sort of caching either in browser or in the server so that each session uses one list that shrinks
 #   over the course of using it
 #       more optimal but more difficult...
+# for now let's just make number 1
