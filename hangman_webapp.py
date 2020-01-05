fef77d2a6e149acb0046c624f7bfe0300022d67d
jcrowe6 <jeremiah.c2001@gmail.com>
2020-01-05 00:44:11 +0000

    several changes

--- hangman_webapp.py
+++ hangman_webapp.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import json
 from datetime import datetime
 import time
@@ -16,13 +16,60 @@ if __name__ == '__main__':
 # Note: if /word is requested again before the first is finished processing, it will get to work on processing again
 # immediately
 @app.route('/word')
-def createSuggestions():
-    toReturn = {}
-    now = datetime.now()
-    toReturn['letters'] = ['A','E','B','K','C']
-    toReturn['words'] = ['apple','change','chains','yellow',str(now)]
+def wordRoute():
+    qstring = request.args.get('word', '')
+    word = qstring.lower()
+    # DUMMY DATA
+    #now = datetime.now()
+    #toReturn['letters'] = ['A','E','B','K','C']
+    #toReturn['words'] = ['apple','change','chains',str(word),str(now)]
+    toReturn = getSuggestions(word)
     return json.dumps(toReturn)
 
+def getSuggestions(word):
+    # Should basically take list of words and iterate through it, checking whether it could match the current word
+    # check: length first, then each KNOWN letter to check that it matches, otherwise, do not append it to the return list
+    # then analyze that list for possible letters
+    log = open('/home/ubuntu/projects/hangman_webapp/log.txt', 'a')
+    log.write(word+'==================\n')
+    toReturn = {'letters':[],'words':[]}
+    maybewords = []
+    length = len(word)
+    with open('/home/ubuntu/projects/hangman_webapp/words_med.txt') as f:
+        allWords = f.read().splitlines()
+        for maybeword in allWords:
+            if len(maybeword) != length:
+                continue
+            else:
+                if not possible(word, maybeword, length, log):
+                    continue
+            maybewords.append(maybeword)
+    #TODO: analyze letter freqs
+
+    maybelen = len(maybewords)
+    #ensure list of words is 10 long
+    if maybelen > 10:
+        temp = []
+        for i in range(10):
+            temp[i] = maybewords[i]
+        maybewords = temp
+    elif maybelen < 10:
+        diff = 10 - maybelen
+        for i in range(diff):
+            maybewords.append('')
+    toReturn['letters'] = ['A','B','C','D','E','F','G','H','I','J']
+    toReturn['words'] = maybewords
+    log.close()
+    return toReturn
+
+def possible(word, maybeword, length, log):
+    log.write(word + ' ' + maybeword + ' ' + str(length) + '\n')
+    for i in range(length):
+        if word[i] == '_':
+            pass
+        elif maybeword[i] != word[i]:
+            return False
+    return True
 # to refactor this:
 # take the existing hangman script and streamline it
 # 2 approaches:
